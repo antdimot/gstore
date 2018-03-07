852ee5bdba4248783d2e5e39f15ce81e1e7def24
Antonio Di Motta <antonio.dimotta@gmail.com>
2018-03-07 22:50:44 +0100

    dev

--- src/db/data.js
+++ src/db/data.js
@@ -18,17 +18,48 @@ var addUsers = ()=> {
 addUsers();
 
 var obj1Id = ObjectId();
+var obj2Id = ObjectId();
+var obj3Id = ObjectId();
+var obj4Id = ObjectId();
 var addData = ()=> {
     db.geodata.drop();
     db.geodata.insert( [
     {
         "_id": obj1Id,
-        "lat": "1",
-        "lon": "1",
-        "content": "Hello World!",
-        "name": "Example",
+        "lat": "40.848082",
+        "lon": "14.283069",
+        "content": "Mate naples site 2",
+        "name": "Mate2",
         "ctype": "text/plain",
         "uid" : user1Id
-    } ] )
+    },
+    {
+        "_id": obj2Id,
+        "lat": "40.850524",
+        "lon": "14.274842",
+        "content": "Hotel Ramada",
+        "name": "Hotel",
+        "ctype": "text/plain",
+        "uid" : user1Id
+    },
+    {
+        "_id": obj3Id,
+        "lat": "40.852815",
+        "lon": "14.271879",
+        "content": "Naples train station",
+        "name": "Station",
+        "ctype": "text/plain",
+        "uid" : user1Id
+    },
+    {
+        "_id": obj4Id,
+        "lat": "40.681562",
+        "lon": "14.774473",
+        "content": "Mate naples site 1",
+        "name": "Mate1",
+        "ctype": "text/plain",
+        "uid" : user1Id
+    },
+ ] )
 };
 addData();
