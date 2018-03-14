e5f78761319b5d6d6eabe55c97a9f12d73f3684e
antdimot <antonio.dimotta@gmail.com>
2018-03-14 16:15:00 +0100

    add web site project

---
+++ src/app/GStore.Web/Models/LoggedUserInfo.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GStore.Web.Models
+{
+    public class LoggedUserInfo
+    {
+        const string _gstoreUser= "gstore.loggeduserinfo";
+
+        // internal properties
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+        public string Username { get; set; }
+
+        // this property is used for showing user on web site
+        public string Display { get; set; }
+
+        private static IHttpContextAccessor httpContextAccessor;
+
+        public static void SetHttpContextAccessor( IHttpContextAccessor accessor )
+        {
+            httpContextAccessor = accessor;
+        }
+
+        public static LoggedUserInfo Current
+        {
+            get
+            {
+                var value = httpContextAccessor.HttpContext.Session.GetString( _gstoreUser );
+
+                var result = value == null ? default( LoggedUserInfo ) : JsonConvert.DeserializeObject<LoggedUserInfo>( value );
+
+                return result;
+            }
+            set
+            {
+                if( value == null )
+                {
+                    httpContextAccessor.HttpContext.Session.Clear();
+                    return;
+                }
+
+                httpContextAccessor.HttpContext
+                                   .Session
+                                   .SetString( _gstoreUser, JsonConvert.SerializeObject( value ) );
+            }
+        }
+    }
+}
---
+++ src/app/GStore.Web/Startup.cs
@@ -0,0 +1,66 @@
+﻿using GStore.Core.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GStore.Web
+{
+    public class Startup
+    {
+        public Startup( IConfiguration configuration )
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication( "GStore" )
+                                .AddCookie( "GStore", options => {
+                                    options.LoginPath = "/Account/LogIn";
+                                    options.LogoutPath = "/Account/LogOff";
+                                } );
+
+            services.AddMvc();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            services.AddSession( options => {
+                // Set a short timeout for easy testing.
+                //options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+            } );
+            services.AddScoped<DataContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
