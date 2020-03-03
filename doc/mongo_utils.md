9d971702ca8eb04534840d447f3f745fe596166f
antdimot <antonio.dimotta@gmail.com>
2020-03-03 17:49:01 +0100

    dev

---
+++ doc/mongo_utils.md
@@ -0,0 +1,4 @@
+
+[The mongo Shell](https://docs.mongodb.com/manual/mongo/)
+
+
--- src/GStore.API/Startup.cs
+++ src/GStore.API/Startup.cs
@@ -3,16 +3,9 @@ using GStore.Core.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
+using Microsoft.Extensions.Hosting;
 
 namespace GStore.API
 {
@@ -46,93 +39,34 @@ namespace GStore.API
                               c.Type == "UserAuthz" && c.Value.Contains("admin") ) ) );
             } );
 
-            services.AddCors();
+            services.AddControllers();
 
-            services.AddMvc( options =>
-            {
-                //options.RequireHttpsPermanent = true;
-            } ).SetCompatibilityVersion( CompatibilityVersion.Version_2_1 );
-
-            services.AddApiVersioning();
-
-            services.AddSwaggerGen( c =>
-            {
-                c.SwaggerDoc( "v1", new Info
-                {
-                    Title = "GStore API",
-                    Description = "A service for storing and retrieving data by latitude and longitude.",
-                    Version = "1",
-                    Contact = new Contact
-                    {
-                        Name = "Antonio Di Motta",
-                        Email = string.Empty,
-                        Url = "https://twitter.com/dimotta"
-                    },
-                } );
-                c.OperationFilter<VersionOperationFilter>();
-                c.DocumentFilter<ApplyDocumentVersionExtensions>();
-                            // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            } );
+            services.AddApiVersioning();           
 
             services.AddScoped<DataContext>();
+
             services.AddScoped<SecurityService>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env )
         {
-            //if( env.IsDevelopment() )
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            if( env.IsDevelopment() )
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            app.UseCors( builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader() );
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //app.UseCors( builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader() );
 
             app.UseAuthentication();
-            app.UseMvc();
 
-            app.UseSwagger( c => { c.RouteTemplate = "api/swagger/{documentName}/swagger.json"; } );
-            app.UseSwaggerUI( c => {
-                c.SwaggerEndpoint( "v1/swagger.json", "GStore API" );
-                c.RoutePrefix = "api/swagger";
-            } );
-        }
-    }
-
-    public class ApiExplorerGroupPerVersionConvention : IControllerModelConvention
-    {
-        public void Apply( ControllerModel controller )
-        {
-            var controllerNamespace = controller.ControllerType.Namespace;
-            var apiVersion = controllerNamespace.Split('.').Last().ToLower();
-
-            controller.ApiExplorer.GroupName = apiVersion;
-        }
-    }
-
-    internal class ApplyDocumentVersionExtensions : IDocumentFilter
-    {
-        public void Apply( SwaggerDocument swaggerDoc, DocumentFilterContext context )
-        {
-            swaggerDoc.Paths = swaggerDoc.Paths
-            .ToDictionary(
-                path => path.Key.Replace( "{version}", swaggerDoc.Info.Version ),
-                path => path.Value
-            );
-        }
-    }
-
-    internal class VersionOperationFilter : IOperationFilter
-    {
-        public void Apply( Operation operation, OperationFilterContext context )
-        {
-            var version = operation.Parameters?.FirstOrDefault(p => p.Name == "version");
-            if( version != null )
+            app.UseEndpoints( endpoints =>
             {
-                operation.Parameters.Remove( version );
-            }
+                endpoints.MapControllers();
+            } );
         }
     }
 }
