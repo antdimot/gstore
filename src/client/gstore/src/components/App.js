43b07df23082b1a4c605e32ea632a20c2d9a5b24
antdimot <antonio.dimotta@gmail.com>
2020-04-09 11:53:53 +0200

    fix docker-compose

---
+++ src/client/gstore/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>GSTore</h1>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default App;
