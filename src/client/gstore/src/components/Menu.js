08e707222ba1ce1bcbc398f30cad2fc57fcc694c
antdimot <antonio.dimotta@gmail.com>
2020-04-14 11:49:40 +0200

    add env, username on client

--- src/client/gstore/src/components/Menu.js
+++ src/client/gstore/src/components/Menu.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 
-import { Navbar,Nav, Button } from 'react-bootstrap';
+import { Navbar,Nav, Form } from 'react-bootstrap';
 import TokenManager from '../helpers/TokenManager';
-
+import DataManager from '../helpers/DataManager'
 
 const Menu = (props) => {
+    const [user,setUser] = useState('pippo');
+
     const logoutHandler = (event) => {
         TokenManager.clearToken();
         props.logoutCallback();
-    } 
+    }
+
+    useEffect(()=> {
+        DataManager().get('/user/info')
+        .then(function (response) {
+            //console.log(response.data);
+            setUser(response.data.firstname + ' ' + response.data.lastname);
+        })
+        .catch(function (error) {
+            console.log(error);
+        }); 
+    },[])
 
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">GSTore</Navbar.Brand>
+            <Navbar.Brand href="#">GSTore</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="/geodatalist">Geodata</Nav.Link>
-                <Button variant="link" size="sm"  onClick={logoutHandler}>
-                    Logout
-                </Button>
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/geodatalist">Geodata</Nav.Link>
+                    <Nav.Link onClick={logoutHandler}>
+                        Logout
+                    </Nav.Link>
                 </Nav>
+                <Form inline>
+                    <Form.Label className="mr-sm-5">{user}</Form.Label>
+                </Form>
             </Navbar.Collapse>
         </Navbar>
     );
--- src/client/gstore/src/helpers/DataManager.js
+++ src/client/gstore/src/helpers/DataManager.js
@@ -6,7 +6,7 @@ const DataManager = () => {
     let access_token = TokenManager.getToken();
 
     return axios.create({
-        baseURL: 'http://localhost:5000/api/v1',
+        baseURL: process.env.REACT_APP_GSTORE_API_BASE_URL, //'http://localhost:5000/api/v1',
         timeout: 10000,
         headers: {
             Authorization: "Bearer " + access_token
