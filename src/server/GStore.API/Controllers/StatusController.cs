1ed336754e6f9be60bd86daa57627f46f0d12c48
Antonio Di Motta <*>
2020-12-26 16:31:42 +0100

    .net 5 migration

--- src/server/GStore.API/Controllers/StatusController.cs
+++ src/server/GStore.API/Controllers/StatusController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GStore.Core.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
